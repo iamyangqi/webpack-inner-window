c8bf79b9fa518aecad3a6c0d58386a44958557f5
qi.yang <qi.yang@transwarp.io>
2019-07-04 12:32:34 +0800

    修改版本

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ class WebpackInnerWindowPlugin {
     injectCode(file) {
         const data = fs.readFileSync(file);
         const scriptSrc = fs.readFileSync(_path.resolve(__dirname, './addInnerWindowScript.txt'));
-        console.log(scriptSrc);
         let content = data.toString();
         content = content.replace(/<\/body>/g, (raw) => {
             return `<script async type="text/javascript">${scriptSrc}</script> \n ${raw}`
